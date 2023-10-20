a620179bb9117e7d12413e53357e2306d4217854
Chobi <watsit777@gmail.com>
2023-10-19 22:13:18 -0600

    V1

--- main.py
+++ main.py
@@ -27,6 +27,29 @@ class Lexico(object):
     def obtenerdescripcion(self):
         return self.tipo
 
+class Lexico(object):
+    def __init__(self, cadena, id, tipo, valor=None):
+        self.cadena = cadena
+        self.token = id
+        self.tipo = tipo
+        self.valor = valor
+
+    def __str__(self):
+        return f"{self.cadena}  {self.token}"
+
+    def obtenercadena(self):
+        return self.cadena
+
+    def obtenertoken(self):
+        return self.token
+
+    def obtenerdescripcion(self):
+        return self.tipo
+
+    def obtenervalor(self):
+        return self.valor
+
+
 class Sintactico(object):
     def __init__(self, listaLexico):
         self.listaLexico = listaLexico
@@ -34,7 +57,18 @@ class Sintactico(object):
         self.error = False
 
     def analizar(self):
-        self.declaracion_variable()
+        while self.posicion < len(self.listaLexico):
+            if self.listaLexico[self.posicion].obtenercadena() in ["int", "float", "char", "void"]:
+                self.declaracion_variable()
+            elif self.listaLexico[self.posicion].obtenercadena() == "if":
+                self.analizar_condicional()
+            elif self.listaLexico[self.posicion].obtenercadena() == "while":
+                self.analizar_bucle()
+            elif self.listaLexico[self.posicion].obtenertoken() == 1:  # ID
+                self.analizar_expresion()
+            else:
+                self.error = True
+                print("Error de sintaxis. Se esperaba una declaración, una expresión o una estructura de control.")
 
     def avanzar(self):
         self.posicion += 1
@@ -51,19 +85,145 @@ class Sintactico(object):
                 valor_esperado = "ID"
             elif token_esperado == 2:
                 valor_esperado = ";"
-            
             row = 0
-            #self.tablaResultados.setItem(row, 0, QtWidgets.QTableWidgetItem(str("Hello")))
             print(f"Error de sintaxis. Se esperaba '{valor_esperado}' en lugar de '{self.listaLexico[self.posicion].obtenercadena()}'")
 
     def declaracion_variable(self):
-        if self.listaLexico[self.posicion].obtenercadena() in ["int", "float", "char", "void"]:
+        tipo_dato = self.listaLexico[self.posicion].obtenercadena()
+        self.avanzar()
+        nombre_variable = self.listaLexico[self.posicion].obtenercadena()
+        self.emparejar(1)  # "ID"
+
+        if self.listaLexico[self.posicion].obtenertoken() == 8:  # "="
             self.avanzar()
-            self.emparejar(1) # "ID"
-            self.emparejar(2) # ";"
+            valor = self.analizar_expresion()
+            self.emparejar(2)  # ";"
+            if valor is not None:
+                print(f"Declaración de variable con inicialización válida: {tipo_dato} {nombre_variable} = {valor};")
         else:
-            self.error = True
-            print("Error de sintaxis. Declaración de variable inválida.")
+            self.emparejar(2)  # ";"
+            print(f"Declaración de variable sin inicialización válida: {tipo_dato} {nombre_variable};")
+
+    def analizar_expresion(self):
+        pila_operadores = []
+        pila_operandos = []
+
+        while self.posicion < len(self.listaLexico):
+            if self.listaLexico[self.posicion].obtenertoken() == 1:  # ID
+                pila_operandos.append(self.listaLexico[self.posicion].obtenercadena())
+                self.avanzar()
+            elif self.listaLexico[self.posicion].obtenertoken() == 13:  # Constante
+                pila_operandos.append(self.listaLexico[self.posicion].obtenervalor())
+                self.avanzar()
+            elif self.listaLexico[self.posicion].obtenertoken() in [14, 16]:  # Operadores de suma o multiplicación
+                while pila_operadores and pila_operadores[-1] in ['+', '-', '*', '/'] and self.precedencia(
+                        self.listaLexico[self.posicion].obtenercadena()) <= self.precedencia(pila_operadores[-1]):
+                    operador = pila_operadores.pop()
+                    operand2 = pila_operandos.pop()
+                    operand1 = pila_operandos.pop()
+                    pila_operandos.append(f"({operand1} {operador} {operand2})")
+                pila_operadores.append(self.listaLexico[self.posicion].obtenercadena())
+                self.avanzar()
+            elif self.listaLexico[self.posicion].obtenertoken() == 4:  # "("
+                pila_operadores.append(self.listaLexico[self.posicion].obtenercadena())
+                self.avanzar()
+            elif self.listaLexico[self.posicion].obtenertoken() == 5:  # ")"
+                while pila_operadores and pila_operadores[-1] != '(':
+                    operador = pila_operadores.pop()
+                    operand2 = pila_operandos.pop()
+                    operand1 = pila_operandos.pop()
+                    pila_operandos.append(f"({operand1} {operador} {operand2})")
+                if pila_operadores and pila_operadores[-1] == '(':
+                    pila_operadores.pop()
+                self.avanzar()
+            elif self.listaLexico[self.posicion].obtenertoken() == 2:  # ";"
+                while pila_operadores:
+                    operador = pila_operadores.pop()
+                    operand2 = pila_operandos.pop()
+                    operand1 = pila_operandos.pop()
+                    pila_operandos.append(f"({operand1} {operador} {operand2})")
+                if len(pila_operandos) == 1:
+                    resultado = pila_operandos[0]
+                    return resultado
+                else:
+                    self.error = True
+                    print("Error de sintaxis en la expresión matemática.")
+                    return None
+            else:
+                self.error = True
+                print("Error de sintaxis en la expresión matemática.")
+                return None
+
+    def precedencia(self, operador):
+        if operador in ['+', '-']:
+            return 1
+        elif operador in ['*', '/']:
+            return 2
+        return 0
+
+    def analizar_condicional(self):
+        self.emparejar(9)  # "if"
+        self.emparejar(4)  # "("
+        self.analizar_expresion()
+        self.emparejar(5)  # ")"
+        # Implementa el análisis del bloque de código dentro del if
+
+    def analizar_bucle(self):
+        self.emparejar(10)  # "while"
+        self.emparejar(4)  # "("
+        self.analizar_expresion()
+        self.emparejar(5)  # ")"
+        # Implementa el análisis del bloque de código dentro del while
+
+    def analizar_expresion(self):
+        # Implementa el análisis de expresiones lógicas
+        pass
+
+# Función para validar y analizar el código
+def analizar_codigo(codigo):
+    listaLexico = []
+
+    i = 0
+    size = len(codigo)
+    cadenaTemp = ""
+
+    while i < size:
+        cadenaTemp = ""
+        while i < size and (codigo[i].isalpha() or codigo[i].isdigit()):
+            cadenaTemp += codigo[i]
+            i += 1
+        if cadenaTemp:
+            if cadenaTemp in ["int", "float", "char", "void", "if", "while"]:
+                listaLexico.append(Lexico(cadenaTemp, 0, "Palabra reservada"))
+            else:
+                listaLexico.append(Lexico(cadenaTemp, 1, "ID"))
+        if i < size and codigo[i] == "=":
+            listaLexico.append(Lexico("=", 8, "Asignación"))
+            i += 1
+        if i < size and codigo[i].isdigit():
+            cadenaTemp = ""
+            while i < size and codigo[i].isdigit():
+                cadenaTemp += codigo[i]
+                i += 1
+            listaLexico.append(Lexico(cadenaTemp, 13, "Constante", cadenaTemp))
+        if i < size and codigo[i] in "+-*/":
+            listaLexico.append(Lexico(codigo[i], 14, "Operador matemático"))
+            i += 1
+        if i < size and codigo[i] in "()":
+            listaLexico.append(Lexico(codigo[i], 4 if codigo[i] == "(" else 5, "Paréntesis"))
+            i += 1
+        if i < size and codigo[i] == ";":
+            listaLexico.append(Lexico(";", 2, "Punto y coma"))
+            i += 1
+
+    analizador_sintactico = Sintactico(listaLexico)
+    analizador_sintactico.analizar()
+
+    if not analizador_sintactico.error:
+        print("Análisis sintáctico completado sin errores.")
+    else:
+        print("Error de sintaxis en el código.")
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
